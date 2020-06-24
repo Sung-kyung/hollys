c155a0ad6c520494239344a6d3238daf5431272a
Sung-kyung <yoo960115@gmail.com>
2020-06-24 18:48:05 +0900

    작업

---
+++ part/head.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>할리스 홈페이지</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css">
+    <link rel="stylesheet" href="/resource/common.css">
+    <link rel="stylesheet" href="/resource/index.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="/resource/common.js"></script>
+</head>
+
+<body>
+
+    <!--탑바-->
+    <div class="top-bar flex flex-jc-sb">
+        <div class="logo flex-ai-c flex"><a href="#" class="flex"><img src="/resource/img/logo.png" alt=""></a></div>
+        <nav class="menu-box-1 flex ">
+            <ul class="flex">
+                <li class="flex"><a href="#" class="flex height-100p flex-ai-c">할리스</a>
+                    <ul>
+                        <li><a href="#" class="block">HOLLYS COFFEE</a></li>
+                        <li><a href="#" class="block">ABOUT COFFEE</a></li>
+                        <li><a href="#" class="block">CSR</a></li>
+                        <li><a href="#" class="block">GALLERY</a></a></li>
+                        <li><a href="#" class="block">혜택안내</a></li>
+                        <li><a href="#" class="block">본사위치</a></li>
+                        <li><a href="#" class="block">채용안내</a></li>
+                        <li><a href="#" class="block">B2B 사업소개</a></li>
+                    </ul>
+                </li>
+                <li class="flex"><a href="#" class="flex height-100p flex-ai-c">메뉴소개</a></li>
+                <li class="flex"><a href="#" class="flex height-100p flex-ai-c">이벤트</a></li>
+                <li class="flex"><a href="#" class="flex height-100p flex-ai-c">커뮤니티</a></li>
+                <li class="flex"><a href="#" class="flex height-100p flex-ai-c">프랜차이즈</a></li>
+            </ul>
+        </nav>
+    </div>
---
+++ public/index.php
@@ -0,0 +1,29 @@
+<?php
+    include "../part/head.php";
+?>
+    <!--배경-->
+    <div class="bg">
+        <div class="main-img-1"><img src="/resource/img/maltesers.png" alt=""></div>
+        <div class="content">
+            <div class="main-title-1 main-title">HOLLYS's</div>
+            <div class="main-title-2 main-title">SEASON MENU</div>
+            <div class="main-title-3 main-title">MALTESERS</div>
+            <div class="main-title-4 main-title">#빙수를_FUN하게 #떤넹수 #몰티져스</div>
+        </div>
+        <div class="discover-box flex">
+            <div class="discover-round-1">
+                <div class="cross"></div>
+            </div>
+            <div class="discover-text flex-as-c">DISCOVER MORE</div>
+
+        </div>
+        <div class="bingsu-img"><img src="/resource/img/bingsu-1.png" alt=""></div>
+    </div>
+    <!--page 2-->
+    <div class="bg-2">
+        <img src="/resource/img/2page_wave.png" alt="">
+        <div class="sub-title"><img src="/resource/img/alwaysBeWithYou.png" alt=""></div>
+    </div>
+<?php
+    include "../part/foot.php";
+?>
